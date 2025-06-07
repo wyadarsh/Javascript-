b12b310e65eb5042b455ed2269d3f2a2dd58bdb1
wyadarsh <148446932+wyadarsh@users.noreply.github.com>
2025-06-07 11:14:58 +0000

    variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,17 @@
+const accountId=144553//const matlab ab wo value dobara change nhi hoga
+let accountEmail="hitesh@google.com"//
+var accountPassword="12345"
+accountCity="jaipur"
+let accountState;
+
+//accountId=34567 //not allowed
+//console.log(accountId);
+
+accountEmail="adarsh@google.com"
+accountPassword="212121"
+accountCity="kolkata"
+console.table([accountEmail,accountPassword,accountCity,accountState])
+/*prefer not to use var keyword
+because of issue in block scope and functional scope*/
+
+
