c182054e5e277c690cc117ef87f62cabaee2d977
wyadarsh <148446932+wyadarsh@users.noreply.github.com>
2025-06-07 11:15:19 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,14 @@
+"use strict";// by writting this all the js code will be written as newer version
+//
+//different types of data types:
+let name="hitesh"//string data type
+let age=13//number data type
+let isLoggedIN=false//boolean data tye true or false
+//null=stand alone value,representation of empty value
+//undefined=jo value define nhi kiya ho
+//symbol=uniqueness find krne ke liye kaam aata h
+//Object
+
+
+console.log(typeof"hitesh");//type of gives info about which type of data type we are using 
+//type of null is object and type of undefined is undefrined**imp.for interview
