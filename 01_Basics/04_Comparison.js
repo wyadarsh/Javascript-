18744e9f9aba4a7671d83cb85beded3fcb1afe4f
wyadarsh <148446932+wyadarsh@users.noreply.github.com>
2025-06-19 11:12:06 +0000

    comparison in java script

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,16 @@
+// the real problem arises when we compare differnet data types
+// console.log("2">1)
+// console.log("02">1)
+// console.log(null==0)
+// console.log(null>0)
+// console.log(null<0)
+// console.log(null>=0)
+// console.log(null<=0)
+// console.log(undefined==0)
+// console.log(undefined>0)
+// console.log(undefined<0)
+// all thes will not give us predictable result  dont out enough mind on it.
+
+
+//strict check(triple equals to se check krna hota h)
+console.log("2"===2);
