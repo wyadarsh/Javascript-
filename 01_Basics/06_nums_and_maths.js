1a8ebe80165d7d951c20e7dfaa8a753b15b62bc3
wyadarsh <148446932+wyadarsh@users.noreply.github.com>
2025-06-29 13:30:02 +0000

    numbers and maths in javascript

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+const score=440;
+// console.log(typeof(score));
+
+const balance=new Number(100)//aise hmlog expicitely define kr kste h ki 100 number hi hoga.
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));//decimal ke bad zero laga dega jitna function ke andar denge
+
+const otherNumber=123.8966
+// console.log(otherNumber.toPrecision(3));//round off krke value dega
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/////*******MATHS*********8 */
+// console.log(Math);
+// console.log(Math.abs(-4));//negative value ko positive me change kr deta h
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));//round off krke upper value milega
+// console.log(Math.floor(4.9));//round off krke lower value milega
+// console.log(Math.min(1,2,3,4));//array me sbse chota value kya h
+// console.log(Math.max(1,2,3,4));//array me sbse bada value kya h
+
+
+//IMPORTANT//
+console.log(Math.random());//math.random ka valUe hmesa 0 aur 1 ke beech me ayega
+console.log((Math.random() *10)+1)
+
+const min=10;
+const max=20;
+
+
+
+
+
+
