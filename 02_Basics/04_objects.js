66b0d5e28f710d860de1b2ef1d5d9da7badf886b
wyadarsh <148446932+wyadarsh@users.noreply.github.com>
2025-07-12 18:38:13 +0000

    object 02 basics

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -9,14 +9,14 @@ const tinderUser={}//non singleton obejct
 tinderUser.id="123abc"
 tinderUser.name="sam"
 tinderUser.isLoggedIn="false"
-console.log(tinderUser);
+//console.log(tinderUser);
 
 //some methods of object:
-console.log(Object.keys(tinderUser));// O/P ka jo data type milega wo array hoga[v.important]
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));//Object.entries() is a method in JavaScript that gives you
+// console.log(Object.keys(tinderUser));// O/P ka jo data type milega wo array hoga[v.important]
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));//Object.entries() is a method in JavaScript that gives you
 //  both the keys and values of an object — in the form of an array of arrays.
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));//gives answer in boolean
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));//gives answer in boolean
 
 
 //objects inside obejct
@@ -40,3 +40,25 @@ const obj2={3:"a",4:"b"}
 const obj3={...obj1,...obj2}
 //console.log(obj3);
 
+//Desturturing:
+const course={
+    coursename:"js in hindi",
+    courseprice:"999",
+    courseinstructor:"Hitesh Chaudhrury"
+}
+const{courseinstructor:instructor}=course
+//console.log(courseinstructor);
+console.log(instructor);
+
+//json:
+{
+    "name":"hitesh",
+    "coursename":"js in hindi",
+    "price":"free"
+}
+//ignore these errors in json the key and value both should be in string
+//json can also be in sorm of array
+[
+    {},
+    {}
+]
